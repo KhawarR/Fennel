322a5351495d71f036fd9035ec43e5f755e22001
Faizan <faizan.dastgir@tintash.com>
2016-09-28 16:45:11 +0500

    enroll

--- app/src/main/java/tintash/fennel/fragments/EnrollFragment.java
+++ app/src/main/java/tintash/fennel/fragments/EnrollFragment.java
@@ -5,15 +5,45 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import tintash.fennel.R;
+import tintash.fennel.views.NothingSelectedSpinnerAdapter;
 
 /**
  * Created by Faizan on 9/27/2016.
  */
 public class EnrollFragment extends BaseContainerFragment {
+
+
+    @Bind(R.id.spLocation)
+    Spinner spLocation;
+
+    @Bind(R.id.spSubLocation)
+    Spinner spSubLocation;
+
+    @Bind(R.id.spVillage)
+    Spinner spVillage;
+
+    @Bind(R.id.spTree)
+    Spinner spTree;
+
+    @Bind(R.id.tvMale)
+    TextView tvMale;
+
+    @Bind(R.id.tvFemale)
+    TextView tvFemale;
+
+    @Bind(R.id.tvLeaderNo)
+    TextView tvLeaderNo;
+
+    @Bind(R.id.tvLeaderYes)
+    TextView tvLeaderYes;
 
 
     @Nullable
@@ -28,6 +58,36 @@ public class EnrollFragment extends BaseContainerFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        System.out.println("ViewCreated Enroll ");
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.optionsLocation, R.layout.simple_spinner_item);
+        spLocation.setAdapter(new NothingSelectedSpinnerAdapter(arrayAdapter, R.layout.spinner_nothing_selected, getContext(), "LOCATION"));
+
+        arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.optionsSubLocation, R.layout.simple_spinner_item);
+        spSubLocation.setAdapter(new NothingSelectedSpinnerAdapter(arrayAdapter, R.layout.spinner_nothing_selected, getContext(), "SUB LOCATION"));
+
+        arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.optionsVillage, R.layout.simple_spinner_item);
+        spVillage.setAdapter(new NothingSelectedSpinnerAdapter(arrayAdapter, R.layout.spinner_nothing_selected, getContext(), "VILLAGE"));
+
+        arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.optionsTree, R.layout.simple_spinner_item);
+        spTree.setAdapter(new NothingSelectedSpinnerAdapter(arrayAdapter, R.layout.spinner_nothing_selected, getContext(), "TREE SPICES"));
+
+
+    }
+
+    @OnClick({R.id.tvMale, R.id.tvFemale})
+    void onClickGenderSelection(View view) {
+        tvFemale.setSelected(false);
+        tvMale.setSelected(false);
+        view.setSelected(true);
+
+    }
+
+    @OnClick({R.id.tvLeaderNo, R.id.tvLeaderYes})
+    void onClickLeaderSelection(View view) {
+        tvLeaderNo.setSelected(false);
+        tvLeaderYes.setSelected(false);
+        view.setSelected(true);
+
     }
 
     @Override
